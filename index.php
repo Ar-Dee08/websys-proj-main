0d082236dc00101866a564399e2dfc31ca1fec62
RD <rdeeflores08@gmail.com>
2024-12-11 00:48:11 +0800

    testing

--- index.php
+++ index.php
@@ -90,6 +90,7 @@
                 <h1>Welcome to Crumbs & Brew Cafe!</h1>
                 <p>Join us and make your coffee and pastry experience even better. Sign up now to start ordering your favorites online!</p>
                 <button id="signUp" type="button">Sign Up</button>
+                <p>GE</p>
             </div>
         </div>
     </div>
