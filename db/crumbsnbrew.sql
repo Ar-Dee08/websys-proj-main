b69d2128f76df0ac5bc688910bd31a184c044c9d
psychirea <atrazograce.000@gmail.com>
2024-12-12 09:42:15 +0800

    updated db

--- db/crumbsnbrew.sql
+++ db/crumbsnbrew.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Dec 11, 2024 at 02:47 PM
+-- Generation Time: Dec 12, 2024 at 02:41 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -24,6 +24,27 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `category_id` int(11) NOT NULL,
+  `category_name` varchar(255) NOT NULL,
+  `category_description` text DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`category_id`, `category_name`, `category_description`, `created_at`, `updated_at`) VALUES
+(1, 'Test 1', 'Agdajas', '2024-12-12 01:31:43', '2024-12-12 01:31:43');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `products`
 --
 
@@ -31,15 +52,18 @@ CREATE TABLE `products` (
   `id` int(11) NOT NULL,
   `product_name` varchar(255) NOT NULL,
   `product_description` text NOT NULL,
-  `product_price` decimal(10,2) NOT NULL
+  `product_price` decimal(10,2) NOT NULL,
+  `category_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `products`
 --
 
-INSERT INTO `products` (`id`, `product_name`, `product_description`, `product_price`) VALUES
-(1, 'Test 1', 'Asdfghjkl', 120.00);
+INSERT INTO `products` (`id`, `product_name`, `product_description`, `product_price`, `category_id`) VALUES
+(1, 'Test 1', 'Asdfghjkl', 120.00, NULL),
+(2, 'Test 2', 'Aswadaxwc', 150.00, NULL),
+(3, 'Test 3', 'Ahshdkskd', 130.00, 1);
 
 -- --------------------------------------------------------
 
@@ -50,7 +74,6 @@ INSERT INTO `products` (`id`, `product_name`, `product_description`, `product_pr
 CREATE TABLE `user_login` (
   `id` int(11) NOT NULL,
   `username` varchar(100) NOT NULL,
-  `dob` date DEFAULT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -59,19 +82,45 @@ CREATE TABLE `user_login` (
 -- Dumping data for table `user_login`
 --
 
-INSERT INTO `user_login` (`id`, `username`, `dob`, `email`, `password`) VALUES
-(1, 'Admin', NULL, 'admin@email.com', '$2y$10$0GntiNCE7lPnwhV.FoQ8c.rsXrjRZIM4AbLISko4LUmvm1hWLsJhi'),
-(2, 'Admin2', NULL, 'admin2@email.com', '$2y$10$GMbl8dGDpeFeT2zs8H9DT.CliznG11aUVZJhchpZ/4bmB7NAb11RS'),
-(3, 'Admin3', NULL, 'admin3@email.com', '$2y$10$UDbBEg.TYhE22geyqbcRsOu1g5jCnwDwy0jK5vIJDPnQ2E1gcRSAS'),
-(4, 'Admin4', NULL, 'admin4@email.com', '$2y$10$Jk4vtj0jMJYFyv8N3ySwEuKKtoGJZ3.n42sSUJbMIvPdmQcJ80D9K'),
-(5, 'Admin5', NULL, 'admin5@email.com', '$2y$10$DLt2m4zCSDOpIhROoiHTs.1Lk8jI6NrSzXqiwp.idZOYXNEsUqqIm'),
-(31, 'Arjon', '2003-07-02', 'aj123@gmail.com', '$2y$10$syNHN0PUGkvmQzpikWukxujEmB7sBeTdaMzTE04dTX4sdp/WfseNK'),
-(33, 'Kurukuru', NULL, 'fd@gmail.com', '$2y$10$nX/exYihfYVWtxQ/0zJZ9uIJ6gUk3L.yhB3Nxg7RfcCKvyIoDjGHC'),
-(35, 'Grace', NULL, 'gra@email.com', '$2y$10$cOBcyFJdfzcdEmInf2pOwuR0wP3SNy1xamNR17NpUneXKZ83x7SjO');
+INSERT INTO `user_login` (`id`, `username`, `email`, `password`) VALUES
+(1, 'Admin', 'admin@email.com', '$2y$10$0GntiNCE7lPnwhV.FoQ8c.rsXrjRZIM4AbLISko4LUmvm1hWLsJhi'),
+(2, 'Admin2', 'admin2@email.com', '$2y$10$GMbl8dGDpeFeT2zs8H9DT.CliznG11aUVZJhchpZ/4bmB7NAb11RS'),
+(3, 'Admin3', 'admin3@email.com', '$2y$10$UDbBEg.TYhE22geyqbcRsOu1g5jCnwDwy0jK5vIJDPnQ2E1gcRSAS'),
+(4, 'Admin4', 'admin4@email.com', '$2y$10$Jk4vtj0jMJYFyv8N3ySwEuKKtoGJZ3.n42sSUJbMIvPdmQcJ80D9K'),
+(5, 'Admin5', 'admin5@email.com', '$2y$10$DLt2m4zCSDOpIhROoiHTs.1Lk8jI6NrSzXqiwp.idZOYXNEsUqqIm'),
+(31, 'Arjon', 'aj123@gmail.com', '$2y$10$syNHN0PUGkvmQzpikWukxujEmB7sBeTdaMzTE04dTX4sdp/WfseNK'),
+(33, 'Kurukuru', 'fd@gmail.com', '$2y$10$nX/exYihfYVWtxQ/0zJZ9uIJ6gUk3L.yhB3Nxg7RfcCKvyIoDjGHC'),
+(36, 'Gra', 'gra@email.com', '$2y$10$BFOBpgpXqsF1UPRCLZjV8uRwQ.ByU02UK18OBJdMDa09Rgeq/xb7q'),
+(37, 'Ewan', 'asfdahjd@email.com', '$2y$10$Mp86JxSocVK30hiGD9H84.j1oS8z.D98Fwy55m5eFEXHpkAWmevSy');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_profile`
+--
+
+CREATE TABLE `user_profile` (
+  `user_id` int(11) NOT NULL,
+  `sex` varchar(10) DEFAULT NULL,
+  `birthdate` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user_profile`
+--
+
+INSERT INTO `user_profile` (`user_id`, `sex`, `birthdate`) VALUES
+(37, 'Male', '2020-01-01');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`category_id`);
 
 --
 -- Indexes for table `products`
@@ -87,20 +136,32 @@ ALTER TABLE `user_login`
   ADD UNIQUE KEY `email` (`email`);
 
 --
+-- Indexes for table `user_profile`
+--
+ALTER TABLE `user_profile`
+  ADD PRIMARY KEY (`user_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `user_login`
 --
 ALTER TABLE `user_login`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
